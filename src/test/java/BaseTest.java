a1926435d5f35798ecead6e93635e0a03a6ca1c7
shumi_kd2r0n5 <shumilov.iv@mail.ru>
2022-12-17 19:27:12 +0300

    added groupTestPage

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,30 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.WebDriver;
+import pages.loginPage.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
+    private static final String LOGIN = "technoPol17";
+    private static final String PASSWORD = "technoPolis2022";
+
     @BeforeAll
-    public void setUp() {
+    public static void setUp() {
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
         Configuration.baseUrl = "https://ok.ru/";
         Configuration.holdBrowserOpen = true;
+    }
+
+    public void logIn() {
+        open(Configuration.baseUrl);
+        new LoginPage()
+                .setLogin(LOGIN)
+                .setPassword(PASSWORD)
+                .submit();
     }
 }
--- src/test/java/GroupsPageTest.java
+++ src/test/java/GroupsPageTest.java
@@ -1,2 +1,25 @@
-public class GroupsPageTest {
+import org.junit.jupiter.api.Test;
+import pages.CurrentGroupPage;
+import pages.groupsPage.GroupCard;
+import pages.groupsPage.GroupsPage;
+import pages.mainPage.MainPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GroupsPageTest extends BaseTest {
+
+    @Test
+    public void testFunc() {
+        logIn();
+        GroupsPage groupsPage = new MainPage().goToGroupsPage();
+        GroupCard firstCard = groupsPage.getFirstGroupCard();
+        String title = firstCard.getTitle(), participAmount = firstCard.getParticipAmount();
+        firstCard.joinGroup();
+        CurrentGroupPage lastGroupPage = groupsPage.goToLastJoinedGroup();
+
+       assertEquals(title, lastGroupPage.getGroupTitle());
+       assertEquals(participAmount, lastGroupPage.getGroupMembersAmount());
+
+    }
 }
