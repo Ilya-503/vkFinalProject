59d9f4d0ee0ceb326165c094e853b1fcf92ff0f9
Ilya Shumilov <shumilov.iv@mail.ru>
2022-12-20 14:54:39 +0300

    added legalData test

--- src/test/java/GroupsPageTest.java
+++ src/test/java/GroupsPageTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.*;
 import pages.LeftNavigatePanel;
+import pages.groupPages.MyGroupPage;
 import pages.groupsPage.GroupCard;
 import pages.groupsPage.GroupsPage;
 import pages.groupsPage.factories.groupPage.GroupPage;
 import pages.groupsPage.pageElements.EventCreationWindow;
 import pages.mainPage.MainPage;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,10 +42,10 @@ public class GroupsPageTest extends BaseTest {
     @DisplayName("Проверка создания мероприятия с неверными данными")
     @ParameterizedTest(name = "ID строки с данными: {0}")
     @CsvFileSource(files = "illegalDataCrEvTest.csv", numLinesToSkip = 1)
-    public void createEventWithIllegalDataTest(String id, String startDate, String phone, String website, String title,
-                         String theme, String city, String address,
-                         String adr_err, String city_err, String thm_err, String title_err,
-                         String ph_err, String web_err, String date_err) throws Exception {
+    public void createEventWithIllegalDataTest(String id, String startDate, String phone, String website,
+                                               String title, String theme, String city, String address,
+                                               String adr_err, String city_err, String thm_err, String title_err,
+                                               String ph_err, String web_err, String date_err) throws Exception {
 
         EventCreationWindow grCrWindow = (EventCreationWindow) groupsPage.createGroup(2);
         grCrWindow
@@ -91,6 +94,37 @@ public class GroupsPageTest extends BaseTest {
                 }
         );
         grCrWindow.closeCreationWindow();
+        }
+
+        @Tags({@Tag("groupsPage"), @Tag("legalData")})
+        @DisplayName("Проверка создания мероприятия с верными данными")
+        @ParameterizedTest(name = "ID передаваемых аргументов: {0}")
+        @MethodSource("provideLegalGrCrParams")
+        public void testLegal(String title, String phone, String website) {
+
+        String startDate = "10.02.2030", address = "address";
+
+        EventCreationWindow grCrWindow = (EventCreationWindow) groupsPage.createGroup(2);
+        grCrWindow
+                .setStartDate(startDate)
+                .setPhone(phone)
+                .setWebSite(website)
+                .setAddress(address)
+                .setTitle(title)
+                .setLegalTheme();
+
+        grCrWindow.setLegalCity();
+        grCrWindow.submitCreation();
+        GroupPage createdGroupPage = new MyGroupPage();
+        assertEquals(title, createdGroupPage.getGroupTitle());
+    }
+
+        private static Stream<Arguments> provideLegalGrCrParams() {
+        return Stream.of(
+                Arguments.of("title1", null, null),
+                Arguments.of("title2", "89509508888", null),
+                Arguments.of("title3", null, "https://ok.ru/")
+        );
         }
 
     @AfterEach
